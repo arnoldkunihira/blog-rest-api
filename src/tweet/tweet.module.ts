07c15e5c6345ecebfd9bb928d5be8b14e923b3a2
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-02-01 13:21:38 +0300

    Configure typeORM module when connecting the tweet entity

---
+++ src/tweet/tweet.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { TweetService } from "./tweet.service";
+import { TweetController } from "./tweet.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Tweet } from "./entities/tweet.entity";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Tweet])],
+    controllers: [TweetController],
+    providers: [TweetService]
+})
+export class TweetModule {}
